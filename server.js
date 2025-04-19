e1a6a1feadeb166d4f3ce12a401b39c5be69ec17
Deepakchandrasekar05 <85093628+Deepakchandrasekar05@users.noreply.github.com>
2025-04-19 21:05:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ let latestGPS= null;
 
 // MySQL Database Connection with Pooling
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "REALG",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
-
 // Check MySQL Connection
 db.getConnection((err, connection) => {
   if (err) {
